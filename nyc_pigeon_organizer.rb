3738098f6f114c89e6ec2b38a888b9ab9a8ada3a
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 12:09:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,4 @@
 def nyc_pigeon_organizer(data)
-  data.each do |key, value|
-    p key
-    if value.is_a?(Hash)
-      value.each do |k, v|
-        p k
-        p v
-      end
-    else
-      p value
-    end
-  end
+  @result = data
+end
   # write your code here!
