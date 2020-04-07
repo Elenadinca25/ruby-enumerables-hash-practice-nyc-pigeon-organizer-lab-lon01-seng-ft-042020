3bf6df6c42b066348855c5628983ad1ed0cdcfd9
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 13:24:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,35 +1,4 @@
 def nyc_pigeon_organizer(data)
-
-  my_array = []
-  pigeon_hash = {}
-
-  data.each do |attribute, items|
-    items.each do |attribute, arr|
-      arr.each do |name|
-        my_array << name if !my_array.include?(name)
-      end
-    end
-  end
-
-  my_array.each do |element|
-    pigeon_hash[element] = Hash.new {|k, v| k[v] = []}
-    data.each do |feauter, items|
-      pigeon_hash[element][attribute]
-      items.each do |attribute, arr|
-        arr.each do |name|
-          pigeon_hash[name][attribute] << feauter.to_s if name == element
-        end
-      end
-    end
-  end
-
-  pigeon_hash
-
-end
-
-
-
-=begin
 new_hash = {}
 data.each do |property, hash|
   hash.each do |attribute, array|
@@ -50,4 +19,3 @@ data.each do |property, hash|
 end
 new_hash
 end
-=end
