1e303466036d8a4d8f44622cbf5d5d583a37644e
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 13:42:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,20 +22,26 @@ new_hash
 end
 =end
 
-piogen_list = {}
-data.each do |key, value|
-  value.each do |describe, array|
-    array.each do |name|
-      if piogen_list.has_key?(name)
-        if piogen_list[name].has_key?(key)
-          piogen_list[name][key] << describe.to.s
-        elsif
-            pigeon_list[name][key] = [describe.to_s]
-          else
-          piogen_list[name] = {key => [describe.to_s]}
+pigeon_list = {}
+data.each do |key, value| #key: color, gender, lives. #value: hash
+  value.each do |describe, array| #describe: purple, male, subway...
+    array.each do |name| #name: index inside aray
+      #check if the empty hash has name inside yet
+      if pigeon_list.has_key?(name)
+        #check if the value of name key has key(color, gender, lives)
+        if pigeon_list[name].has_key?(key)
+        #if name has key already, add the describe value at the current iteration
+          pigeon_list[name][key] << describe.to_s
+        #if name has no key at the current iteration yet, set the new hash
+        else
+          pigeon_list[name][key] = [describe.to_s]
         end
+      #set name level 1 -> key level 2 with describe as value of the hash
+      else
+        pigeon_list[name] = {key => [describe.to_s]} #describe was symbol before so turn it to string
       end
     end
   end
-  piogen_list
+end
+pigeon_list
 end
