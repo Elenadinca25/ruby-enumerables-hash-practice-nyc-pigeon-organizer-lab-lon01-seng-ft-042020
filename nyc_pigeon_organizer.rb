2e10470fce1abafdb726bf0c38238a43819edfbf
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 13:38:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,7 +30,7 @@ data.each do |key, value|
         if piogen_list[name].has_key?(key)
           piogen_list[name][key] << describe.to.s
         else
-          piogen_list[name]= {[key] = [describe.to_s]}
+          piogen_list[name] = {key => [describe.to_s]}
         end
       end
     end
