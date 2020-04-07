a429456db0fde06a1cf6f526674be01fa2e8ad2a
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 13:17:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 def nyc_pigeon_organizer(data)
-=begin
+
   names = []
   pigeon_hash = {}
 
@@ -11,13 +11,13 @@ def nyc_pigeon_organizer(data)
     end
   end
 
-  names.each do |nombre|
-    pigeon_hash[nombre] = Hash.new {|k, v| k[v] = []}
+  names.each do |element|
+    pigeon_hash[element] = Hash.new {|k, v| k[v] = []}
     data.each do |attribute, items|
-      pigeon_hash[nombre][attribute]
+      pigeon_hash[element][attribute]
       items.each do |feature, arr|
         arr.each do |name|
-          pigeon_hash[name][attribute] << feature.to_s if name == nombre
+          pigeon_hash[name][attribute] << feature.to_s if name == element
         end
       end
     end
@@ -26,47 +26,7 @@ def nyc_pigeon_organizer(data)
   pigeon_hash
 
 end
-=end
 
-new_hash = {}
-  data.each do |first_level, all_levels|
-    all_levels.each do |category, array|
-      array.each do |name|
-        new_hash[name] = {:color => [], :gender => [], :lives => []}
-      end
-    end
-  end
-  x = new_hash.keys
-  data[:color].each do |bird_color, name|
-    name.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          new_hash[item][:color] << bird_color.to_s
-        end
-      end
-    end
-  end
-  data[:gender].each do |gender, type|
-    type.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          new_hash[item][:gender] << gender.to_s
-        end
-      end
-    end
-  end
-  data[:lives].each do |location, name|
-    name.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          new_hash[item][:lives] << location
-        end
-      end
-    end
-  end
-
-  return new_hash
-end
 
 
 =begin
