66337cb6fba45ecfaf21a3ebd577fa3a4ed4b085
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 12:21:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,8 @@
 def nyc_pigeon_organizer(data)
   @result = data
-    data.each { |pair| p pair }
+    data.reduce({}) do { |memo, pair| }
+      p memo
+      p pair 
+      memo
 end
   # write your code here!
